a35c7e72a874c7b34186951fd29768a9940fdd00
Matias <157364130+Cofre-dev@users.noreply.github.com>
2025-05-15 19:28:36 -0400

    Añadiendo el proyecto de spring
    hola

---
+++ ms-webpay-amb/src/main/java/cl/duoc/ms_webpay_amb/MsWebpayAmbApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_webpay_amb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsWebpayAmbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsWebpayAmbApplication.class, args);
+	}
+
+}
---
+++ ms-webpay-amb/src/test/java/cl/duoc/ms_webpay_amb/MsWebpayAmbApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_webpay_amb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsWebpayAmbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
